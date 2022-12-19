14264fdb2fde5de44fd0afbb98bc8d689cb0174b
salmanbarani <salman.barani@postpay.io>
2022-12-19 13:39:35 +0300

    end up with auth service

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -1,6 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
+interface UserPayload {
+    id: string;
+    email: string;
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: UserPayload;
+        }
+    }
+}
+
 
 export const currentUser = (req: Request, res: Response, next: NextFunction) => {
 
@@ -9,7 +22,7 @@ export const currentUser = (req: Request, res: Response, next: NextFunction) => 
     }
 
     try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_TOKEN!)
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
         req.currentUser = payload;
     }catch (err){
         // Pass
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,25 +1,11 @@
 import express  from "express";
-import Jwt  from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router()
 
-router.get("/api/users/currentuser", (req, res) => {
-    if (!req.session?.jwt){
-        return res.send({currentUser: null});
-    }
-
-    try{
-        const currentUser = Jwt.verify(
-            req.session.jwt,
-            process.env.JWT_KEY!
-        );
-        res.send({currentUser: currentUser});
-
-    }catch (err) {
-        res.send({currentUser: null});
-    }
-
-
+router.get("/api/users/currentuser", currentUser, requireAuth, (req, res) => {
+    res.send({currentUser: req.currentUser || null});
 });
 
 
