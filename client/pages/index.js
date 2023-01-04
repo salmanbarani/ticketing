891282635923c17381a38088f6c38e65e6cd7ed2
salmanbarani <salman.barani@postpay.io>
2023-01-04 13:03:45 +0300

    testing server side fetching

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,14 @@
-const Index = () => {
+import axios from "axios";
+
+const LandingPage =  ({currentUser}) => {
+    // console.log(currentUser);
+    axios.get("/api/users/currentuser");
     return <h1>Landing Page</h1>;
   };
 
+LandingPage.getInitialProps = async () => {
+//   const response = await axios.get("/api/users/currentuser");
+  return {currentUser: "something"};
+}
 
-export default Index
+export default LandingPage;
